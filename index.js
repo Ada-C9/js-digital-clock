a4af3c8c0ed469408cc06f6450bb78c1f7544793
Zheng Cao <chori84@gmail.com>
2018-05-18 15:29:00 -0700

    Add update function

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const currentTime = new Date(Date.now())
-const hr = currentTime.getHours();
-const min = currentTime.getMinutes();
-const sec = currentTime.getSeconds();
+function updateTime() {
+  const currentTime = new Date(Date.now())
+  const hr = currentTime.getHours();
+  const min = currentTime.getMinutes();
+  const sec = currentTime.getSeconds();
 
-$('#clock').append(`<span>${hr} : </span>`);
-$('#clock').append(`<span>${min} : </span>`);
-$('#clock').append(`<span>${sec}</span>`);
+  $('#clock').replaceWith(`<div id='clock'>${hr} : ${min} : ${sec}</div>`);
+}
+
+const myTimer = setInterval(updateTime, 1000);
