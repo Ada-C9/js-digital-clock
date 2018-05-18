5737faef1f15d80dc3d9b39c8f428c5bd964f217
Monalisa Chatterjee <usermonalisa@gmail.com>
2018-05-18 16:28:15 -0700

    add code

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code here
+$(document).ready(function () {
+  window.setInterval(myCallback, 1000);
+  function myCallback() {
+    $('#clock').html(new Date(Date.now()).toLocaleTimeString('en-US'));
+  }
+});
