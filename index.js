091b955043752f367446bed02640cf06079f9b83
knockknockhusthere <angela.huxia@gmail.com>
2018-05-18 15:37:03 -0700

    dried up the code

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-// Your code here
 
-
-$(document).ready(function() {
 function displayTime() {
   let currentTime = new Date();
 
@@ -15,6 +12,7 @@ function displayTime() {
   if (sec < 10){
     sec = "0" + sec;
   }
+
   let time_str = hr + ":" + min + ":" + sec;
 
   if(hr > 11){
@@ -23,11 +21,9 @@ function displayTime() {
     time_str += " AM";
   }
 
-  let clock = document.getElementById('clock')
-  clock.innerText = time_str;
+document.getElementById('clock').innerText = time_str;
 }
 
-
-
+$(document).ready(function() {
   setInterval(displayTime, 1000);
 });
