678828626a5bd9bd8fe47be0f97618ea28df4357
Hannah Cameron <hannahiscoding@gmail.com>
2018-05-18 15:13:21 -0700

    got current time to print w/o date info

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code here
+
+const showTime = () => {
+  let time = new Date();
+
+  // console.log(time.toLocaleTimeString('en-US'));
+  // console.log(event.toLocaleTimeString('en-US'));
+  $('#clock').append(time.toLocaleTimeString('en-US'));
+};
+
+$(document).ready(() => {
+  showTime();
+});
