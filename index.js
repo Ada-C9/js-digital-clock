190f2ee82d56d25eb46bc9861f5a455fd31a647e
Anne <annehwatson@gmail.com>
2018-05-18 15:15:09 -0700

    seconds refreshing with interval

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // Use the Date Library in Javascript to retrieve the current date and time information. Use the different methods that are provided to you for retrieving the individual hour, minute and second information.
 
 //Use the setInterval method to utilize the function you've created to update each second.
-console.log('Running javascript');
+const reset = function reset() {
+  let interval = window.setInterval(displayTime, 1000);
+}
 
 
 const displayTime = function displayTime() {
@@ -9,10 +11,13 @@ const displayTime = function displayTime() {
   let localTime = date.toLocaleTimeString('en-US');
 
 
-  $('#clock').append(localTime);
+  $('#clock').html(localTime);
 
 };
 
+
+
 $(document).ready(() => {
-  displayTime();
+  reset();
+  // displayTime();
 });
