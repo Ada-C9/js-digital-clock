48da3ac8b446fc3234745813202204047375cdbf
Anne <annehwatson@gmail.com>
2018-05-18 15:08:28 -0700

    displaying clock

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 // Use the Date Library in Javascript to retrieve the current date and time information. Use the different methods that are provided to you for retrieving the individual hour, minute and second information.
 
 //Use the setInterval method to utilize the function you've created to update each second.
+console.log('Running javascript');
 
 
 const displayTime = function displayTime() {
   let date = new Date();
   let localTime = date.toLocaleTimeString('en-US');
-  console.log(localTime);
-  // let hours = date.getHours();
-  // let minutes = date.getMinutes();
-  // console.log(hours + ':' + minutes);
 
-}
 
-console.log(displayTime());
+  $('#clock').append(localTime);
+
+};
+
+$(document).ready(() => {
+  displayTime();
+});
