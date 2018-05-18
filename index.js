4a4ec52bc98555727f060d848d8bc66879a79eef
Zheng Cao <chori84@gmail.com>
2018-05-18 15:09:31 -0700

    Show time without update

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code here
+const currentTime = new Date(Date.now())
+const hr = currentTime.getHours();
+const min = currentTime.getMinutes();
+const sec = currentTime.getSeconds();
+
+$('#clock').append(`<span>${hr} : </span>`);
+$('#clock').append(`<span>${min} : </span>`);
+$('#clock').append(`<span>${sec}</span>`);
