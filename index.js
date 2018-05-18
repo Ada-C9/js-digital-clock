a724d3f3b16156035b5b5cec82354435b7c2640f
Jackie <jonofre16@hotmail.com>
2018-05-18 16:01:31 -0700

    Added javascript function read time to display the formatted date and time

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Your code here
+function readTime() {
+let dt = new Date();
+
+let month = new Array();
+month[0] = "January";
+month[1] = "February";
+month[2] = "March";
+month[3] = "April";
+month[4] = "May";
+month[5] = "June";
+month[6] = "July";
+month[7] = "August";
+month[8] = "September";
+month[9] = "October";
+month[10] = "November";
+month[11] = "December";
+
+let m = month[dt.getMonth()];
+let d = dt.getDay();
+let y = dt.getFullYear();
+let hr = dt.getHours();
+let min = dt.getMinutes();
+let sec = dt.getSeconds();
+
+
+let display = m + ' ' + d + ',' + ' ' + y + '<br>' + hr + ":" + min + ":" + sec;
+$('#clock').append(`${display}`);
+}
+
+setInterval(readTime, 1000);
+clearInterval(readTime);
