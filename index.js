f730ae4d5f665671c94ef71023b74249b5213aa6
Abiaina <abinnet.a@gmail.com>
2018-05-18 16:21:29 -0700

    time and date function on html with jquery

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Your code here
+// It automatically do a date nowwhen called so it doessn matter that I don't use date.now right?
+let date = new Date()
+
+const getMonthName = function getMonthName(num){
+  const months = ['January', 'February', 'March', 'April', 'May', 'June',
+  'July', 'August', 'September', 'October', 'November', 'December'];
+
+  return months[num];
+}
+
+//this function works with an integer in the console
+const getNominalHours = function getNominalHours(num){
+  return (num > 12 ? (num - 12) : num);
+}
+
+const getNominalMinutes = function getNominalMinutes(num){
+  return (num < 10 ? `0${num}` : num);
+}
+
+let legibleDate = `${getMonthName(date.getMonth())} ${date.getDay()},
+${date.getFullYear()}`;
+
+let legibleTime = `${getNominalHours(date.getHours())} :
+${getNominalMinutes(date.getMinutes())} : ${date.getSeconds()}`;
+
+$('#clock').append(`<p>${legibleDate}</p>`);
+$('#clock').append(`<p>${legibleTime}</p>`);
+
+
+// or just use Date.prototype.getTime()
+console.log(legibleDate);
+console.log(legibleTime);
