ca38bd407b285d3bf6e970c4ffdca98518fbb98a
Kate Pond <kate@thepondsedge.com>
2018-05-18 16:44:44 -0700

    makes clock actually count with setInterval

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-$( document ).ready(function() {
-const hour = new Date().getHours();
-const minute = new Date().getMinutes();
-const second = new Date().getSeconds();
+$( document ).ready(() => {
 
-$('#clock').append(`${hour}:${minute}:${second}`);
+  const clock = function clock() {
+    const hour = new Date().getHours();
+    const minute = new Date().getMinutes();
+    const second = new Date().getSeconds();
 
+    const time = `${hour}:${minute}:${second}`
+
+    $('#clock').html(`${time}`);
+  };
+  
+  window.setInterval(clock, 1000);
 });
