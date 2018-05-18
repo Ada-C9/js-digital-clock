58c2abd124149b4f33c8630d3a9869305320a784
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-05-18 14:58:40 -0700

    Modify jQuery to display only necessary date/time information

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // Your code here
 $(document).ready(() => {
   setInterval(function() {
-    let rightNow = new Date();
-    $('#clock').html(rightNow);
-  });
+    let dateToday = new Date();
+    let date = dateToday.getDate() + '/' + dateToday.getMonth() + '/' + dateToday.getFullYear();
+    $('#clock').html(date);
+    let rightNow = dateToday.getHours() + ':' + dateToday.getMinutes() + ':' + dateToday.getSeconds();
+    $('#clock').append('\n' + rightNow);
+  }, 1000);
 });
