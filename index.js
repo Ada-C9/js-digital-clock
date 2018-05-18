e45701eaee0dc3a94d3cf2b9e6fa8379f53ae720
Ana Lisa Sutherland <sutherlandanalisa@gmail.com>
2018-05-18 15:43:44 -0700

    Update time logic currently displaying a correct time

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Your code here
+// first we need to get the damn current time
+const updateTime = () => {
+  let currentTime = new Date ();
+
+  // breaks it down into hours, mins, secs
+  let currentSeconds = currentTime.getSeconds();
+  let currentMinutes = currentTime.getMinutes();
+  let currentHours = currentTime.getHours();
+
+  //need to format the time 00:00:00
+  if (currentSeconds < 10) {
+    currentSeconds = "0" + currentSeconds;
+  }
+
+  if (currentMinutes < 10) {
+    currentMinutes = "0" + currentMinutes;
+  }
+
+  let amPM = null
+  // Gonna need to know if it is AM or pm
+  if (currentHours < 12) {
+    amPM = 'AM';
+  } else {
+    amPM = 'PM';
+  }
+
+  // Need to know that 0 is noon/midnight
+  if (currentHours === 0 ) {
+    currentHours = 12;
+  }
+
+  // display time
+  let currentTimeDisplay = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + amPM;
+
+  // update the time
+  console.log(currentTimeDisplay);
+}
+
+updateTime();
