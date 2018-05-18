dab1675c07d0eabdf355def554652b28bca96fd9
Kate Pond <kate@thepondsedge.com>
2018-05-18 15:31:19 -0700

    adds jquery and static clock

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code here
+$( document ).ready(function() {
+const hour = new Date().getHours();
+const minute = new Date().getMinutes();
+const second = new Date().getSeconds();
+
+$('#clock').append(`${hour}:${minute}:${second}`);
+
+});
