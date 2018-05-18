84d1b516caec582b9ce2fb36f9547af0f78de895
kseastman <kat.eastman@gmail.com>
2018-05-18 14:44:11 -0700

    working example

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+// class Clock {
+//   constructor() {
+//     this.date = this.setDate();
+//     this.hour = this.date.getHours();
+//     this.minute = this.date.getMinutes();
+//     this.second = this.date.getSeconds();
+//   }
+//   setDate() {
+//     this.date = new Date();
+//     this.hour = this.date.getHours();
+//     this.minute = this.date.getMinutes();
+//     this.second = this.date.getSeconds();
+//     return this.date
+//   }
+// }
+
+const myClock = () => {
+    let date = new Date();
+    document.getElementById("clock").innerHTML = date.toLocaleTimeString();
+};
+
+let mySeconds = setInterval(myClock, 1000)
+
+
+$(document).ready(function() {
+  myClock();
+  mySeconds;
+});
