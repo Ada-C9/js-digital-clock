074825d8d9f3ffd03f11fb20a7ed5eb8863af291
knockknockhusthere <angela.huxia@gmail.com>
2018-05-18 15:33:55 -0700

    works

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+
+
+$(document).ready(function() {
+function displayTime() {
+  let currentTime = new Date();
+
+  let hr = currentTime.getHours();
+  let min = currentTime.getMinutes();
+  let sec = currentTime.getSeconds();
+
+  if (min < 10){
+    min = "0" + min
+  }
+  if (sec < 10){
+    sec = "0" + sec;
+  }
+  let time_str = hr + ":" + min + ":" + sec;
+
+  if(hr > 11){
+    time_str += " PM";
+  } else {
+    time_str += " AM";
+  }
+
+  let clock = document.getElementById('clock')
+  clock.innerText = time_str;
+}
+
+
+
+  setInterval(displayTime, 1000);
+});
