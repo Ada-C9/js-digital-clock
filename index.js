23526b5d79d8f58935ea8d57c72e373748bc933f
Maja Graonic <m.graonic@gmail.com>
2018-05-18 15:56:04 -0700

    clock finished

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code here
+const getTime = () => {
+  const dateThing = new Date();
+
+  let displayDate = `${dateThing.toLocaleDateString()}; ${dateThing.getHours()}:${dateThing.getMinutes()}:${dateThing.getSeconds()}`;
+  $('#clock').html(displayDate);
+};
+
+
+
+
+
+$(document).ready(() => {
+  // setInterval takes function and milliseconds as argument
+  setInterval(getTime, 1000);
+});
