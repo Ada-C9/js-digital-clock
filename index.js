0dd172351ed30462a63bdfa3108c34222d72902f
Brandy Austin <brandy.austin.seattle@gmail.com>
2018-05-18 16:13:40 -0700

    Working clock with added formatting

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Your code here
+const monthNames = ["January", "February", "March", "April", "May", "June",
+"July", "August", "September", "October", "November", "December"];
+
+function addZero(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
+const getDate = () => {
+  const today = new Date();
+
+  let month = monthNames[today.getMonth()];
+  let formattedDate = `${month} ${today.getDay()}, ${today.getFullYear()}`;
+
+  $('#date').html(formattedDate);
+
+  let hours = today.getHours();
+  let minutes = addZero(today.getMinutes());
+  let seconds = addZero(today.getSeconds());
+
+  let formattedTime = `${hours}:${minutes}:${seconds}`;
+
+  $('#time').html(formattedTime);
+}
+
+window.setInterval(getDate, 1000);
+
+
+// const getDate = () => {
+//
+//   let today = new Date();
+//   $('#clock').html(today);
+// }
+//
+// window.setInterval(getDate, 1000);
