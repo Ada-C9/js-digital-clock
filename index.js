257c567b4fe607f92457ab96808f158667cb2b5e
Anne <annehwatson@gmail.com>
2018-05-18 14:49:02 -0700

    initial function setup

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code here
+// Use the Date Library in Javascript to retrieve the current date and time information. Use the different methods that are provided to you for retrieving the individual hour, minute and second information.
+
+//Use the setInterval method to utilize the function you've created to update each second.
+
+
+const displayTime = function displayTime() {
+  let date = new Date();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  console.log(hours + ':' + minutes);
+
+}
+
+console.log(displayTime());
