2aae6e3bce5df31ab62634af7e7561a871cab574
Samantha Berk <sberk10@gmail.com>
2018-05-18 15:40:41 -0700

    slice the GMT-0700 off the date object

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 let dateTime = new Date();
+
+// set the interval to increment by 1 second
 const interval = 1000;
 
 function updateTime() {
   dateTime = new Date(dateTime.getTime() + interval);
-  $('#clock').html(dateTime.toString());
+  
+  // slice the GMT-0700 off the date object
+  $('#clock').html(dateTime.toString().split(' ').slice(0, 5).join(' '));
 }
 
-$(document).ready(function() {
+$(document).ready(() => {
   updateTime();
   setInterval(updateTime, interval);
 });
