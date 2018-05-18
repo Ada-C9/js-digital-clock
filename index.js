c28c4f759c158881461cdd041aef1e0806cbe46d
Zheng Cao <chori84@gmail.com>
2018-05-18 15:52:07 -0700

    Show year month date day

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 function updateTime() {
-  const currentTime = new Date(Date.now())
-  const hr = currentTime.getHours();
-  const min = currentTime.getMinutes();
-  const sec = currentTime.getSeconds();
+  const currentTimeString = new Date(Date.now()).toString();
+  // const yr = currentTime.getFullYear();
+  // const mon = currentTime.getMonth();
+  // const date = currentTime.getDate();
+  // const day = currentTime.getDay();
+  // const hr = currentTime.getHours();
+  // const min = currentTime.getMinutes();
+  // const sec = currentTime.getSeconds();
 
-  $('#clock').replaceWith(`<div id='clock'>${hr} : ${min} : ${sec}</div>`);
+  // const dateString = new Date(Date.now()).toDateString();
+  // const timeString = new Date(Date.now()).toTimeString();
+
+  $('#clock').replaceWith(`<div id="clock">${currentTimeString}</div>`);
+  // $('#clock').replaceWith(`<div id='clock'>${day} ${mon + 1} ${date} ${yr} ${hr} : ${min} : ${sec}</div>`);
+  // $('#date').replaceWith(`<div id='date'>${dateString}</div>`);
+  // $('#time').replaceWith(`<div id='time'>${timeString}</div>`);
+
 }
 
 const myTimer = setInterval(updateTime, 1000);
