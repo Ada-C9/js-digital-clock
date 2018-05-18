fb73b5d58ae8ab1f4f4ec70f5d1371323456d449
Cara <cararbrennan@gmail.com>
2018-05-18 16:00:36 -0700

    index.js and index.html working together

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+// let target = document.getElementById('clock');  // Find the HTML element where the ID is js-lecture-target
+// target.innerHTML = '<p>Here is the clock!</p>';
+//   const now = new Date();
+//   let day = now.getDays();
+//   target.innerHTML = `<li> date ${day}</li>`;
+
+
+const dateDisplay = () => {
+  console.log("in date display");
+  const nows = new Date(Date.now());
+  console.log(nows);
+
+
+  let day = nows.getDay();
+  console.log(day);
+
+  let hour = nows.getHours();
+  console.log(hour);
+  let minute = nows.getMinutes();
+  let second = nows.getSeconds();
+
+
+  $('#clock').html(nows);
+  $('#day').html(day);
+  $('#hour').html(hour);
+  $('#minute').html(minute);
+  $('#second').html(second);
+};
+
+$(document).ready(() => {
+  dateDisplay();
+});
