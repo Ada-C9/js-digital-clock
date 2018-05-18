04fde78e8057e1db41496c40ae9984a913c4cdc8
Steffany Brown <steffannbrown@gmail.com>
2018-05-18 15:22:57 -0700

    initial date setup

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+
+// $(document).ready(function() {
+  let date = new Date
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = '0' + minutes;
+  }
+
+  let seconds =date.getSeconds();
+  if (seconds < 10) {
+    seconds = '0' + seconds;
+  }
+
+const target = document.getElementById('clock');
+target.innerHTML = `The time is...<h1> ${hours}:${minutes}:${seconds} </h1>`;
+
+// })
