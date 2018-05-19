c034ecc45f59fcf9553445a5e2b5dddb49d3c36f
Zheng Cao <chori84@gmail.com>
2018-05-18 20:14:33 -0700

    Add four time zone function

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 function updateTime() {
-  const currentTimeString = new Date(Date.now()).toString();
-  // const yr = currentTime.getFullYear();
-  // const mon = currentTime.getMonth();
-  // const date = currentTime.getDate();
-  // const day = currentTime.getDay();
-  // const hr = currentTime.getHours();
-  // const min = currentTime.getMinutes();
-  // const sec = currentTime.getSeconds();
+  const currentDateTime = new Date(Date.now());
 
-  // const dateString = new Date(Date.now()).toDateString();
-  // const timeString = new Date(Date.now()).toTimeString();
+  const sTimeString = currentDateTime.toLocaleString('en-US', { timeZone: 'America/Los_Angeles' });
+  $('.clock#seattle').replaceWith(`<div class="clock" id="seattle"><p>Seattle</p><p>${sTimeString}</p></div>`);
 
-  $('#clock').replaceWith(`<div id="clock">${currentTimeString}</div>`);
-  // $('#clock').replaceWith(`<div id='clock'>${day} ${mon + 1} ${date} ${yr} ${hr} : ${min} : ${sec}</div>`);
-  // $('#date').replaceWith(`<div id='date'>${dateString}</div>`);
-  // $('#time').replaceWith(`<div id='time'>${timeString}</div>`);
+  const nyTimeString = currentDateTime.toLocaleString('en-US', { timeZone: 'America/New_York' });
+  $('.clock#new-york').replaceWith(`<div class="clock" id="new-york"><p>New York</p><p>${nyTimeString}</p></div>`);
 
+  const shTimeString = currentDateTime.toLocaleString('zh-CN', { timeZone: 'Asia/Shanghai' });
+  $('.clock#shanghai').replaceWith(`<div class="clock" id="shanghai"><p>上海</p><p>${shTimeString}</p></div>`);
+
+  const ldTimeString = currentDateTime.toLocaleString('en-GB', { timeZone: 'Europe/London' });
+  $('.clock#london').replaceWith(`<div class="clock" id="london"><p>London</p><p>${ldTimeString}</p></div>`);
 }
 
 const myTimer = setInterval(updateTime, 1000);
