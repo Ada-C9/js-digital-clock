285d166bb5391bb5859d620b6948eb0d18cc2639
Cheeron Mars <cheeronmars+gh@posteo.org>
2018-05-18 19:06:28 -0700

    created digital clock

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Your code here
+$(document).ready(() => {
+  function currentTime() {
+    let date = new Date();
+    let time = date.toLocaleTimeString();
+    $('#clock').text(time);
+  }
+ window.setInterval(currentTime, 1000);
+
+})
