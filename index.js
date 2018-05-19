c92be43d5c67f0d8452bd606c156a002daaab2c3
Maja Graonic <m.graonic@gmail.com>
2018-05-18 17:00:05 -0700

    now displays two places for hours minutes and seconds

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
+const twoDigitTime = (time) => {
+  let newDateTime = time;
+
+  if (newDateTime < 10) {
+    return (`0${newDateTime}`);
+  } else {
+    return newDateTime;
+  }
+};
+
 const getTime = () => {
   const dateThing = new Date();
 
-  let displayDate = `${dateThing.toLocaleDateString()}; ${dateThing.getHours()}:${dateThing.getMinutes()}:${dateThing.getSeconds()}`;
+  let hours = twoDigitTime(dateThing.getHours());
+  let minutes = twoDigitTime(dateThing.getMinutes());
+  let seconds = twoDigitTime(dateThing.getSeconds());
+
+  let displayDate = `${dateThing.toLocaleDateString()} ${hours}:${minutes}:${seconds}`;
   $('#clock').html(displayDate);
 };
 
