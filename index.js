990024e8e2d8ad7ce5ecf61d63ef76eb92cba1b2
0velez0 <mariaivelez@gmail.com>
2018-05-19 13:03:52 -0700

    add time string updating every second with no jQuery

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code here
+// dateObj.setSeconds(secondsValue)
+
+function myClock() {
+  let date = new Date();
+  let timeString = date.toLocaleTimeString();
+
+  let clock = document.getElementById("clock");
+  clock.textContent = timeString;
+}
+
+let myInterval = setInterval(function() {
+  myClock();
+}, 1000);
+
+$(document).ready(function() {
+  myInterval();
+});
