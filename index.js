3e2aef413f75dd90d29c55b1c4114b34853f08c4
Dikla <dikla.aharoni@gmail.com>
2018-05-18 17:19:36 -0700

    Added id to index.html file

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+const MONTH = ["January", "February",
+ "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+const DAY = ["Sunday", "Monday", "Tuesday", "Wednesday","Thursday", "Friday", "Saturday"];
+
+
+
+const digitalClock = () => {
+  let clock = new Date();
+  let time = clock.toLocaleTimeString();
+  $('#clock').html(time);
+}
+
+
+setInterval(digitalClock, 1000);
+
+const date = () => {
+  let clock = new Date();
+  let month = clock.getMonth();
+  let day = clock.getDate();
+  let year = clock.getFullYear();
+
+  $('#date').append(`${MONTH[month]} ${day} ${year}`);
+}
+
+const day = () => {
+  let clock = new Date();
+  let weekDay = clock.getDay();
+  $('#day').append(`${DAY[weekDay]}`);
+}
+
+$(document).ready(() => {
+  digitalClock();
+  date();
+  day();
+});
