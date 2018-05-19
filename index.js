2f8f0e1e4af6647b29c12ad10854cd335ef6aedf
Luxi Lindsey <Luxi.Lindsey@gmail.com>
2018-05-18 17:16:17 -0700

    Created functions and methods to display the date and time

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
-// Your code here
+
+const MONTH = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+const DAY = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
 const digitalClock = () => {
+  let date = new Date();
 
-  let date = Date.now();
+  let display = date.toLocaleTimeString();
 
-  console.log(date);
-
+  $('#clock').html(display);
 };
 
+setInterval(digitalClock, 1000);
+
+const displayDate = () => {
+  let date = new Date();
+
+  let weekDay = date.getDay();
+  let month = date.getMonth();
+  let day = date.getDate();
+  let year = date.getFullYear();
+
+  $('#date').append(`${DAY[weekDay]}, ${MONTH[month]} ${day} ${year}`);
+};
 
 $(document).ready(() => {
   digitalClock();
+  displayDate();
 });
