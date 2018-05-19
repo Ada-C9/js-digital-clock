2614060a6b1c23f96e96186b33aaea0dfb899739
Nora Peters <norapeters5@gmail.com>
2018-05-19 15:56:30 -0700

    adds leadingzeros function, refactoring. clock is operational in browser

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Your code here
+const addLeadingZero = function addLeadingZero(num) {
+  return ('0'+num).slice(-2);
+}
+
+const updateDate = function updateDate(){
+  let date = new Date();
+  let hour = date.getHours();
+  if (hour > 12) {
+    hour -= 12;
+  }
+  let minutes = addLeadingZero(date.getMinutes());
+  let seconds = addLeadingZero(date.getSeconds());
+  let today = date.toDateString();
+  let dateNow = $('<p>' + today + '</p>' + '<p>' + hour + ":" + minutes + ":" + seconds + '</p>')
+  $('#clock').html(dateNow);
+}
+
+$(document).ready(function() {
+
+  setInterval(function(){ updateDate() }, 1000);
+
+});
