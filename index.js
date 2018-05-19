ce3dc7b15beb40d50708d03ada95188597ddf28a
Wenjie <traxiaojie@gmail.com>
2018-05-18 18:13:39 -0700

    Added timeBuilder to retrieve info from date Added setInterval function

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+const MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September",
+"October", "November", "December"];
+
+const WEEKS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+
+const timeBuilder = () => {
+  let seattleTime = new Date();
+  let time = '';
+  time += seattleTime.getFullYear();
+  time += ' ' + MONTHS[seattleTime.getMonth() - 1];
+  time += ' ' + seattleTime.getDate();
+  time += ',' + WEEKS[seattleTime.getDay()];
+  time += ' ' + seattleTime.getHours();
+  time += ':' + seattleTime.getMinutes();
+  time += ':' + seattleTime.getSeconds();
+  $('#clock').html(time);
+};
+
+let runClock = setInterval(timeBuilder, 1000);
+
+$(document).ready( () => {
+  $('#clock').html(runClock);
+})
