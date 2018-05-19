2a81a9836ce11cf75cc48105a98af5556488613c
Kiera <kiera.thomasson@gmail.com>
2018-05-18 17:48:48 -0700

    added functionality to day function. displays the month date and time with interval seconds.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+const date = () => {
+  let today = new Date();
+  let month = today.getMonth();
+  let day = today.getDay();
+  let numDay = today.getDate();
+  let year = today.getFullYear();
+  let hr = today.getHours();
+  let min = today.getMinutes();
+  let sec = today.getSeconds();
+
+  let tick = setInterval(date,10);
+
+  const timeRightNow = ` ${months[month]} ${numDay}, ${year} ${hr}:${min}:${sec}`;
+  $('#clock').html(timeRightNow);
+
+};
+
+
+
+$(document).ready(() => {
+  date();
+
+});
