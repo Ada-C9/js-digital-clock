f31020b9bd233eb20b54986db609a641eff84380
Kaitlin Forsman <kcforsman@outlook.com>
2018-05-18 21:10:02 -0700

    finished this minimum requirements to get an operational clock that updates the time every second using the Date objects in JavaScript and setInterval, also added a function that allows you to convert the time displayed between a 12-hour and 24-hour clock

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+$(document).ready(function() {
+
+  $('#time-type').click( function() {
+    $( this ).toggleClass('to-military');
+    if ( $( this ).hasClass('to-military') ) {
+      $( this ).html('<p>Change to 24-hr Clock</p>');
+    } else {
+      $( this ).html('<p>Change to 12-hr Clock</p>');
+    }
+  });
+
+  setInterval( getTime, 1000);
+
+  function getTime() {
+    const currentDateTime = new Date(Date.now());
+    let hours = currentDateTime.getHours();
+    if ( $('#time-type').hasClass('to-military') && hours > 12) {
+      hours = currentDateTime.getHours() - 12;
+    } else if ( $('#time-type').hasClass('to-military') && hours === 0) {
+      hours = 12;
+    }
+    const minutes = currentDateTime.getMinutes();
+    const seconds = currentDateTime.getSeconds();
+    const date = currentDateTime.toDateString()
+
+    $('#clock').html(`<p><strong>${hours} : ${minutes} : ${seconds}</strong></p><p>${date}</p>`);
+  }
+
+});
