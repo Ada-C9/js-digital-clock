bdf9b87ff2bcb553acc9b7854a247a3f900a5642
Lily Sky <lilyxsky@gmail.com>
2018-05-20 16:08:54 -0700

    good to go

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+
+
+$(document).ready(() => {
+  const displayTime = function displayTime() {
+    let today = new Date();
+
+    let hours = today.getHours();
+    let minutes = today.getMinutes();
+    let seconds = today.getSeconds();
+
+    if ( seconds < 10 ) {
+      seconds = '0' + seconds
+    }
+
+
+    let clockDiv = document.getElementById('clock');
+
+    clockDiv.innerText = hours + ":" + minutes + ":" + seconds;
+
+
+  }
+
+  setInterval(displayTime, 1000);
+})
