9d76567882811e2feea5b79f17f7aa2df7683ba9
Samantha Berk <sberk10@gmail.com>
2018-05-19 18:28:50 -0700

    add shanghai time zone

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-let SeattleTime = new Date();
+// Set time Zones
 
-// set the interval to increment by 1 second
-const interval = 1000;
+// Seattle time
+let SeattleTime = new Date();
 
 // DC time
 let DCTime = new Date();
@@ -13,22 +13,34 @@ let CairoTime = new Date();
 let CairoOffset = 9*60;
 CairoTime.setMinutes(CairoTime.getMinutes() + CairoOffset);
 
+// Shanghai time
+let ShanghaiTime = new Date();
+let ShanghaiOffset = 15*60;
+ShanghaiTime.setMinutes(ShanghaiTime.getMinutes() + ShanghaiOffset);
+
+// set the interval to increment by 1 second
+const interval = 1000;
 
 function updateTime() {
   SeattleTime = new Date(SeattleTime.getTime() + interval);
 
   // slice the GMT-0700 off the date object
-  $('#seattle').html(`Seattle: <br>${SeattleTime.toString().split(' ').slice(0, 5).join(' ')}`);
+  $('#seattle').html(`Seattle, WA: <br>${SeattleTime.toString().split(' ').slice(0, 5).join(' ')}`);
 }
 
 function updateDCTime() {
   DCTime = new Date(DCTime.getTime() + interval);
-  $('#dc').html(DCTime.toString().split(' ').slice(0, 5).join(' '));
+  $('#dc').html(`Washington, DC: <br>${DCTime.toString().split(' ').slice(0, 5).join(' ')}`);
 }
 
 function updateCairoTime() {
   CairoTime = new Date(CairoTime.getTime() + interval);
-  $('#cairo').html(CairoTime.toString().split(' ').slice(0, 5).join(' '));
+  $('#cairo').html(`Cairo, Egypt: <br>${CairoTime.toString().split(' ').slice(0, 5).join(' ')}`);
+}
+
+function updateShanghaiTime() {
+  ShanghaiTime = new Date(ShanghaiTime.getTime() + interval);
+  $('#shanghai').html(`Shanghai, China: <br>${ShanghaiTime.toString().split(' ').slice(0, 5).join(' ')}`);
 }
 
 $(document).ready(() => {
@@ -44,4 +56,9 @@ $(document).ready(() => {
 $(document).ready(() => {
   updateCairoTime();
   setInterval(updateCairoTime, interval);
+});
+
+$(document).ready(() => {
+  updateShanghaiTime();
+  setInterval(updateShanghaiTime, interval);
 });
