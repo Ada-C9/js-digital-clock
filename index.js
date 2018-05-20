c0ce2af0c33ce0d39202a736d8e34612ecc976a0
Winifred Irarrazaval <winirarrazaval@gmail.com>
2018-05-19 23:02:24 -0700

    Added 4 different time zones,  and then made a for Each loop so I dont repeat the js code

--- index.js
+++ index.js
@@ -1,1 +1,64 @@
-// Your code here
+
+
+const WEEKDAY = [`Sun`, `Mon`, `Tue`, `Wed`, `Thur`, `Fri`, `Sat`]
+
+let currenttime = new Date();
+
+$(`#date`).html(
+  WEEKDAY[currenttime.getDay()] +
+  `,` + currenttime.getMonth() + "/" +
+  currenttime.getDate() + "/" + currenttime.getFullYear()
+);
+
+
+const updateClock = () => {
+  let currentdate = new Date();
+  $(`#clock`).html(
+    currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds()
+  );
+  $(`#clock`).append(
+    "<br>Seattle</br>"
+  );
+}
+
+
+const LOCATIONS = [
+  {place: `Chile`, hours: 3},
+  {place: `London`,  hours: 8},
+  {place: `Tokio`, hours: 16}
+];
+
+
+  const updateChile = () => {
+    LOCATIONS.forEach((location) => {
+      let chileTime = new Date();
+      chileTime.setHours(chileTime.getHours() + location.hours);
+      $(`#`+location.place.toLowerCase()+`clock`).html(
+        chileTime.getHours() + ":" + chileTime.getMinutes() + ":" + chileTime.getSeconds()
+      );
+      $(`#`+location.place.toLowerCase()+`clock`).append(
+        "<br>" + location.place + "</br>"
+      );}
+    );}
+
+  //   let chileTime = new Date();
+  //   chileTime.setHours(chileTime.getHours() + 3);
+  //   $(`#chileclock`).html(
+  //     chileTime.getHours() + ":" + chileTime.getMinutes() + ":" + chileTime.getSeconds()
+  //   );
+  //   $(`#chileclock`).append(
+  //     "<br>Santiago, Chile</br>"
+  //   );
+  // }
+
+
+  setInterval(updateClock, 1000);
+  setInterval(updateChile, 1000);
+
+
+
+
+  $(document).ready(()=> {
+    updateClock();
+    updateChile();
+  });
