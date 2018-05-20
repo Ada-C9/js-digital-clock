b1e95ac16e7637d0879fc09b2fd0c0234f9312ad
Samantha Berk <sberk10@gmail.com>
2018-05-19 17:39:35 -0700

    DC time updated to 3 hours ahead of Seattle Local time

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-let dateTime = new Date();
+let SeattleTime = new Date();
 
 // set the interval to increment by 1 second
 const interval = 1000;
 
+
+let DCTime = new Date();
+
+
 function updateTime() {
-  dateTime = new Date(dateTime.getTime() + interval);
-  
+  SeattleTime = new Date(SeattleTime.getTime() + interval);
+
   // slice the GMT-0700 off the date object
-  $('#clock').html(dateTime.toString().split(' ').slice(0, 5).join(' '));
+  $('#clock').html(SeattleTime.toString().split(' ').slice(0, 5).join(' '));
+}
+
+function updateDCTime() {
+  DCTime.setHours(SeattleTime.getHours() + 3 ) ;
+  DCTime = new Date(DCTime.getTime() + interval);
+
+  $('#DC').html(DCTime.toString().split(' ').slice(0, 5).join(' '));
 }
 
 $(document).ready(() => {
   updateTime();
   setInterval(updateTime, interval);
+});
+
+$(document).ready(() => {
+  updateDCTime();
+  setInterval(updateDCTime, interval);
 });
