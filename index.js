ec9e0f9ebe54b6da2b9dfe7ea70bdcae636b09f4
Jamila <J.cornick21@gmail.com>
2018-05-19 18:14:09 -0700

    basic clock works

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+let time = new Date();
+let intervalID = setInterval(() => {
+  $('#clock').html(new Date())
+}, 1000);
+
+
+// function updateTime() {
+// $('#clock').append(time)
+// }
+
+console.log('YougotTime');
+$(document).ready(() => {
+  console.log(intervalID);
+
+});
