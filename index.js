ee46dd9c728c02f13f09e3bfba6b986643f7fc3e
Abiaina <abinnet.a@gmail.com>
2018-05-19 18:33:42 -0700

    time updates on its own. changed function call to an anonymous function

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// It automatically do a date nowwhen called so it doessn matter that I don't use date.now right?
-let date = new Date()
 
 const getMonthName = function getMonthName(num){
   const months = ['January', 'February', 'March', 'April', 'May', 'June',
@@ -8,34 +6,40 @@ const getMonthName = function getMonthName(num){
   return months[num];
 }
 
-//this function works with an integer in the console
 const getNominalHours = function getNominalHours(num){
   return (num > 12 ? (num - 12) : num);
 }
 
-const getNominalMinutes = function getNominalMinutes(num){
+const getNominalMinutesSeconds = function getNominalMinutesSeconds(num){
   return (num < 10 ? `0${num}` : num);
 }
 
-let legibleDate = `${getMonthName(date.getMonth())} ${date.getDay()},
-${date.getFullYear()}`;
+setInterval(function () {
+  let date = new Date()
 
-let legibleTime = `${getNominalHours(date.getHours())} :
-${getNominalMinutes(date.getMinutes())} : ${date.getSeconds()}`;
+  let legibleDate = `${getMonthName(date.getMonth())} ${date.getDay()},
+  ${date.getFullYear()}`;
 
-// $('#clock').append(`<p>${legibleDate}</p>`);
-// $('#clock').append(`<p>${legibleTime}</p>`);
-
-
-function updateDateTime() {
-  // $('#clock').empty()
-
+  let legibleTime = `${getNominalHours(date.getHours())} :
+  ${getNominalMinutesSeconds(date.getMinutes())} : ${getNominalMinutesSeconds(date.getSeconds())}`;
+  $('#clock').empty();
   $('#clock').append(`<p>${legibleDate}</p>`);
   $('#clock').append(`<p>${legibleTime}</p>`);
-}
+}, 1000);
 
-setInterval(updateDateTime(), 1000);
+// function displayDateTime() {
+//   let date = new Date()
+//
+//   let legibleDate = `${getMonthName(date.getMonth())} ${date.getDay()},
+//   ${date.getFullYear()}`;
+//
+//   let legibleTime = `${getNominalHours(date.getHours())} :
+//   ${getNominalMinutesSeconds(date.getMinutes())} : ${getNominalMinutesSeconds(date.getSeconds())}`;
+//   // $('#clock').empty();
+//   $('#clock').append(`<p>${legibleDate}</p>`);
+//   $('#clock').append(`<p>${legibleTime}</p>`);
+// }
+
+// setInterval(displayDateTime(), 1000);
 
 // or just use Date.prototype.getTime()
-console.log(legibleDate);
-console.log(legibleTime);
