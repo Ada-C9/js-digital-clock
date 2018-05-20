64519cbc431ef926d1c7f6beb3c5e1198db60db4
Samantha Berk <sberk10@gmail.com>
2018-05-19 18:20:57 -0700

    changed timezone format to utc time to account for difference in days

--- index.js
+++ index.js
@@ -3,22 +3,32 @@ let SeattleTime = new Date();
 // set the interval to increment by 1 second
 const interval = 1000;
 
-
+// DC time
 let DCTime = new Date();
+let DCOffset = 3*60;
+DCTime.setMinutes(DCTime.getMinutes() + DCOffset);
+
+// Cairo time
+let CairoTime = new Date();
+let CairoOffset = 9*60;
+CairoTime.setMinutes(CairoTime.getMinutes() + CairoOffset);
 
 
 function updateTime() {
   SeattleTime = new Date(SeattleTime.getTime() + interval);
 
   // slice the GMT-0700 off the date object
-  $('#clock').html(SeattleTime.toString().split(' ').slice(0, 5).join(' '));
+  $('#seattle').html(`Seattle: <br>${SeattleTime.toString().split(' ').slice(0, 5).join(' ')}`);
 }
 
 function updateDCTime() {
-  DCTime.setHours(SeattleTime.getHours() + 3 ) ;
   DCTime = new Date(DCTime.getTime() + interval);
+  $('#dc').html(DCTime.toString().split(' ').slice(0, 5).join(' '));
+}
 
-  $('#DC').html(DCTime.toString().split(' ').slice(0, 5).join(' '));
+function updateCairoTime() {
+  CairoTime = new Date(CairoTime.getTime() + interval);
+  $('#cairo').html(CairoTime.toString().split(' ').slice(0, 5).join(' '));
 }
 
 $(document).ready(() => {
@@ -28,5 +38,10 @@ $(document).ready(() => {
 
 $(document).ready(() => {
   updateDCTime();
-  setInterval(updateDCTime, interval);
+  setInterval(updateDCTime, 1000);
+});
+
+$(document).ready(() => {
+  updateCairoTime();
+  setInterval(updateCairoTime, interval);
 });
