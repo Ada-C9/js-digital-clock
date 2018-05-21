4dcb33b53dde4cc7f8f75382374511cb36bcd4c9
Victoria Garcia <v.elisabeth.garcia@gmail.com>
2018-05-21 07:42:42 -0700

    commented out an unnecessary line of Javascript

--- index.js
+++ index.js
@@ -11,44 +11,9 @@ function myTimer() {
   const minutes = current.getMinutes();
   const seconds = current.getSeconds();
 
-  const readout = `${hours}:${minutes}:${seconds}`
+  // const readout = `${hours}:${minutes}:${seconds}`
 
 
 
   $('#clock').html(`${hours}:${minutes}:${seconds}`)
 }
-console.log('whatever')
-console.log(`${myTimer.readout}`)
-// });
-
-// let target = document.getElementById('clock');  //
-//
-// let now = Date.now();
-// let nowDate = new Date(now);
-// let nowHour = nowDate.getHours();
-// let nowMinute = nowDate.getMinutes();
-// let nowSecond = nowDate.getSeconds();
-// let timeReport = `${nowHour}:${nowMinute}:${nowSecond}`
-
-
-// const Timer = function Timer(now) {
-//
-//   this.nowDate = new Date(now);
-//   this.nowHour = nowDate.getHours();
-//   this.nowMinute = nowDate.getMinutes();
-//   this.nowSecond = nowDate.getSeconds();
-//   this.timeReport = `${this.nowHour}:${this.nowMinute}:${this.nowSecond}`    }
-//
-//   Timer.prototype = {
-//     readOut() {
-//     return Timer.timeReport
-//   }
-// };
-
-// let myTimer = new Timer(Date.now);
-// let reading = myTimer.timeReport;
-// console.log(reading);
-//
-// console.log(myTimer.nowDate)
-
-// target.innerHTML = timeReport
