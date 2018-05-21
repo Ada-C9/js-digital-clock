a15e84a7a489dba5286c01923def25cbd865bdf6
Samantha Berk <sberk10@gmail.com>
2018-05-21 13:26:15 -0700

    toggle button to change time format

--- index.js
+++ index.js
@@ -42,40 +42,49 @@ function updateShanghaiTime() {
   ShanghaiTime = new Date(ShanghaiTime.getTime() + interval);
   $('#shanghai').html(`<strong>Shanghai, China: </strong><br>
     ${ShanghaiTime.toString().split(' ').slice(0, 5).join(' ')}`);
-}
+  }
 
-function format12HourTime(time) {
-  let hours = time.getHours();
-  let minutes = time.getMinutes();
-  let ampm = hours >= 12 ? 'pm' : 'am';
-  hours = hours % 12;
-  hours = hours ? hours : 12;
-  minutes = minutes < 10 ? '0' + minutes : minutes;
-  let strTime = time.toString().split(' ').slice(1, 4).join(' ');
-  strTime = strTime + hours + ':' + minutes + ' ' + ampm;
-  return strTime;
-}
+  function format12HourTime(time) {
+    let hours = time.getHours();
+    let minutes = time.getMinutes();
+    let ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    let strTime = time.toString().split(' ').slice(1, 4).join(' ');
+    strTime = strTime + hours + ':' + minutes + ' ' + ampm;
+    return strTime;
+  }
 
-$(document).ready(() => {
-  updateTime();
-  setInterval(updateTime, interval);
-  updateDCTime();
-  setInterval(updateDCTime, interval);
-  updateCairoTime();
-  setInterval(updateCairoTime, interval);
-  updateShanghaiTime();
-  setInterval(updateShanghaiTime, interval);
-});
-
-$(document).ready(() => {
-  $('button').click ( function() {
-    if ($(this).html() === 'Lilac') {
-      $('#border').css('background-color', '#D7BDE2');
-    } else if ($(this).html() === 'Pink') {
-      $('#border').css('background-color', '#F5B7B1');
-    } else if ($(this).html() === 'Blue') {
-      $('#border').css('background-color', '#5D8CAE');
-    }
-    // alert (`Got a click on a button "${$(this).html()}"`);
+  $(document).ready(() => {
+    updateTime();
+    setInterval(updateTime, interval);
+    updateDCTime();
+    setInterval(updateDCTime, interval);
+    updateCairoTime();
+    setInterval(updateCairoTime, interval);
+    updateShanghaiTime();
+    setInterval(updateShanghaiTime, interval);
   });
-});
+
+  $(document).ready(() => {
+    $('button').click ( function() {
+      if ($(this).html() === 'Lilac') {
+        $('#border').css('background-color', '#D7BDE2');
+      } else if ($(this).html() === 'Pink') {
+        $('#border').css('background-color', '#F5B7B1');
+      } else if ($(this).html() === 'Blue') {
+        $('#border').css('background-color', '#5D8CAE');
+      }
+    });
+  });
+
+  $(document).ready(() => {
+    $('.time-btn').click ( function() {
+      if($(this).html() === '12 hours') {
+        $(this).html('24 hours');
+      } else {
+        $(this).html('12 hours')
+      }
+    });
+  });
