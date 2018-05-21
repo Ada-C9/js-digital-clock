216950604b36b1d1ed4c6b1cf703bd91ce7c2c2c
Victoria Garcia <v.elisabeth.garcia@gmail.com>
2018-05-21 07:39:49 -0700

    After copius help from Chris, this thing is now working.

--- index.js
+++ index.js
@@ -1,16 +1,54 @@
 // Your code here
 
+$(document).ready(function() {
 
-console.log('This is a test');
+  setInterval( myTimer, 1000);
+});
 
-let target = document.getElementById('clock');  // Find the HTML element where the ID is js-lecture-target
+function myTimer() {
+  const current = new Date();
+  const hours = current.getHours();
+  const minutes = current.getMinutes();
+  const seconds = current.getSeconds();
 
-let now = Date.now();
-let nowDate = new Date(now);
-let nowHour = nowDate.getHours();
-let nowMinute = nowDate.getMinutes();
-let nowSecond = nowDate.getSeconds();
+  const readout = `${hours}:${minutes}:${seconds}`
 
-let timeReport = `${nowHour}:${nowMinute}:${nowSecond}`
 
-target.innerHTML = timeReport
+
+  $('#clock').html(`${hours}:${minutes}:${seconds}`)
+}
+console.log('whatever')
+console.log(`${myTimer.readout}`)
+// });
+
+// let target = document.getElementById('clock');  //
+//
+// let now = Date.now();
+// let nowDate = new Date(now);
+// let nowHour = nowDate.getHours();
+// let nowMinute = nowDate.getMinutes();
+// let nowSecond = nowDate.getSeconds();
+// let timeReport = `${nowHour}:${nowMinute}:${nowSecond}`
+
+
+// const Timer = function Timer(now) {
+//
+//   this.nowDate = new Date(now);
+//   this.nowHour = nowDate.getHours();
+//   this.nowMinute = nowDate.getMinutes();
+//   this.nowSecond = nowDate.getSeconds();
+//   this.timeReport = `${this.nowHour}:${this.nowMinute}:${this.nowSecond}`    }
+//
+//   Timer.prototype = {
+//     readOut() {
+//     return Timer.timeReport
+//   }
+// };
+
+// let myTimer = new Timer(Date.now);
+// let reading = myTimer.timeReport;
+// console.log(reading);
+//
+// console.log(myTimer.nowDate)
+
+// target.innerHTML = timeReport
