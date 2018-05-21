82ff8ba352b482cf16664945ac4a10cd3c984979
AngelaPoland <angelalpoland@gmail.com>
2018-05-21 13:22:40 -0700

    adds color changing buttons

--- index.js
+++ index.js
@@ -5,15 +5,34 @@ setInterval(function(){ myTimer() }, 1000);
 
   function myTimer() {
     let d = new Date();
-    let time = d.toLocaleTimeString();
+    // let time = d.toLocaleTimeString(); //this is not military time
+    let time = d.toLocaleTimeString('en-US', {hour12: false}); //this is military time
 
     let month = d.getMonth()+1;
     let day = d.getDate();
+
 
     let output = (month<10 ? '0' : '') + month + '/' + (day<10 ? '0' : '') + day
     + '/' + d.getFullYear() + ' ' + time;
 
     $('#clock').html(output);
 }
+
+  $('.blue').click(function() {
+    $('#clock').css({ 'color' : 'blue' });
+    })
+
+  $('.pink').click(function() {
+    $('#clock').css({ 'color' : '#D804DC'});
+  })
+
+  $('.lavender').click(function() {
+    $('#clock').css({ 'color' : '#AF83B0'});
+  })
+
+  $('.time').click(function() {
+    // code that toggles to/from military time.
+    alert('this should toggle the time to/from military time')
+  })
 
 })
