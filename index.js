33063782f4ffcd93995e4b2fe9c6c16a50df630e
Abiaina <abinnet.a@gmail.com>
2018-05-21 15:13:04 -0700

    fixed day for date, added text for change color buttons

--- index.js
+++ index.js
@@ -1,97 +1,99 @@
 
-const getMonthName = function getMonthName(num){
-  const months = ['January', 'February', 'March', 'April', 'May', 'June',
-  'July', 'August', 'September', 'October', 'November', 'December'];
 
-  return months[num];
-}
+$(document).ready(() => {
+  const getMonthName = function getMonthName(num){
+    const months = ['January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'];
 
-const getNominalHours = function getNominalHours(num){
-  return (num > 12 ? (num - 12) : num);
-}
+    return months[num];
+  }
 
-const getNominalMinutesSeconds = function getNominalMinutesSeconds(num){
-  return (num < 10 ? `0${num}` : num);
-}
+  const getNominalHours = function getNominalHours(num){
+    return (num > 12 ? (num - 12) : num);
+  }
 
-setInterval(function () {
-  let date1 = new Date()
+  const getNominalMinutesSeconds = function getNominalMinutesSeconds(num){
+    return (num < 10 ? `0${num}` : num);
+  }
 
-  let legibleDate1 = `${getMonthName(date1.getMonth())} ${date1.getDay()},
-  ${date1.getFullYear()}`;
+  setInterval(function () {
+    let date = new Date();
 
-  let legibleTime1 = `${getNominalHours(date1.getHours())} :
-  ${getNominalMinutesSeconds(date1.getMinutes())} : ${getNominalMinutesSeconds(date1.getSeconds())}`;
+    let legibleDate = `${getMonthName(date.getMonth())} ${date.getDate()}, ${date.getFullYear()}`;
 
-  $('#clock1').empty();
-  $('#clock1').append('<li>Pacific US Time</li>')
-  $('#clock1').append(`<li>${legibleDate1}</li>`);
-  $('#clock1').append(`<li>${legibleTime1}</li>`);
+    let legibleTime = `${getNominalHours(date.getHours())} :
+    ${getNominalMinutesSeconds(date.getMinutes())} : ${getNominalMinutesSeconds(date.getSeconds())}`;
 
-  let date2 = new Date()
-  // do I have to reset this value to date2?
-  //why doesnt it work like this:
-  // date2 = date2.setHours(date2.getHours() + 3);
-  date2.setHours(date2.getHours() + 3);
+    $('#clock1').empty();
+    $('#clock1').append('<li>Pacific US Time</li>')
+    $('#clock1').append(`<li>${legibleDate}</li>`);
+    $('#clock1').append(`<li>${legibleTime}</li>`);
 
-
-  let legibleDate2 = `${getMonthName(date2.getMonth())} ${date2.getDay()},
-  ${date2.getFullYear()}`;
-
-  let legibleTime2 = `${getNominalHours(date2.getHours())} :
-  ${getNominalMinutesSeconds(date2.getMinutes())} : ${getNominalMinutesSeconds(date2.getSeconds())}`;
-
-  $('#clock2').empty();
-  $('#clock2').append('<li>Eastern US Time</li>')
-  $('#clock2').append(`<li>${legibleDate2}</li>`);
-  $('#clock2').append(`<li>${legibleTime2}</li>`);
-
-  let date3 = new Date()
-  date3.setHours(date3.getHours() + 7);
+    let date2 = new Date()
+    // do I have to reset this value to date2?
+    //why doesnt it work like this:
+    // date2 = date2.setHours(date2.getHours() + 3);
+    date2.setHours(date2.getHours() + 3);
 
 
-  let legibleDate3 = `${getMonthName(date3.getMonth())} ${date3.getDay()},
-  ${date3.getFullYear()}`;
+    let legibleDate2 = `${getMonthName(date2.getMonth())} ${date2.getDate()},
+    ${date2.getFullYear()}`;
 
-  let legibleTime3 = `${getNominalHours(date3.getHours())} :
-  ${getNominalMinutesSeconds(date3.getMinutes())} : ${getNominalMinutesSeconds(date3.getSeconds())}`;
+    let legibleTime2 = `${getNominalHours(date2.getHours())} :
+    ${getNominalMinutesSeconds(date2.getMinutes())} : ${getNominalMinutesSeconds(date2.getSeconds())}`;
 
-  $('#clock3').empty();
-  $('#clock3').append('<li>UTC Time Zone</li>')
-  $('#clock3').append(`<li>${legibleDate3}</li>`);
-  $('#clock3').append(`<li>${legibleTime3}</li>`);
+    $('#clock2').empty();
+    $('#clock2').append('<li>Eastern US Time</li>')
+    $('#clock2').append(`<li>${legibleDate2}</li>`);
+    $('#clock2').append(`<li>${legibleTime2}</li>`);
 
-  let date4 = new Date()
-  date4.setHours(date4.getHours() + 9);
-
-  let legibleDate4 = `${getMonthName(date4.getMonth())} ${date4.getDay()},
-  ${date4.getFullYear()}`;
-
-  let legibleTime4 = `${getNominalHours(date4.getHours())} :
-  ${getNominalMinutesSeconds(date4.getMinutes())} : ${getNominalMinutesSeconds(date4.getSeconds())}`;
-
-  $('#clock4').empty();
-  $('#clock4').append('<li>Troll Station Antartica Time</li>')
-  $('#clock4').append(`<li>${legibleDate4}</li>`);
-  $('#clock4').append(`<li>${legibleTime4}</li>`);
-}, 1000);
+    let date3 = new Date()
+    date3.setHours(date3.getHours() + 7);
 
 
+    let legibleDate3 = `${getMonthName(date3.getMonth())} ${date3.getDate()},
+    ${date3.getFullYear()}`;
 
-//why doesnt this work ? :
-// function displayDateTime() {
-//   let date = new Date()
-//
-//   let legibleDate = `${getMonthName(date.getMonth())} ${date.getDay()},
-//   ${date.getFullYear()}`;
-//
-//   let legibleTime = `${getNominalHours(date.getHours())} :
-//   ${getNominalMinutesSeconds(date.getMinutes())} : ${getNominalMinutesSeconds(date.getSeconds())}`;
-//   // $('#clock').empty();
-//   $('#clock').append(`<p>${legibleDate}</p>`);
-//   $('#clock').append(`<p>${legibleTime}</p>`);
-// }
+    let legibleTime3 = `${getNominalHours(date3.getHours())} :
+    ${getNominalMinutesSeconds(date3.getMinutes())} : ${getNominalMinutesSeconds(date3.getSeconds())}`;
 
-// setInterval(displayDateTime(), 1000);
+    $('#clock3').empty();
+    $('#clock3').append('<li>UTC Time Zone</li>')
+    $('#clock3').append(`<li>${legibleDate3}</li>`);
+    $('#clock3').append(`<li>${legibleTime3}</li>`);
 
-// or just use Date.prototype.getTime()
+    let date4 = new Date()
+    date4.setHours(date4.getHours() + 9);
+
+    let legibleDate4 = `${getMonthName(date4.getMonth())} ${date4.getDate()},
+    ${date4.getFullYear()}`;
+
+    let legibleTime4 = `${getNominalHours(date4.getHours())} :
+    ${getNominalMinutesSeconds(date4.getMinutes())} : ${getNominalMinutesSeconds(date4.getSeconds())}`;
+
+    $('#clock4').empty();
+    $('#clock4').append('<li>Troll Station Antartica Time</li>')
+    $('#clock4').append(`<li>${legibleDate4}</li>`);
+    $('#clock4').append(`<li>${legibleTime4}</li>`);
+  }, 1000);
+
+  $('#red').click( function() {
+    $('.clock').removeClass();
+    $('#clock').addClass('red');
+  });
+
+  $('#black').click( function() {
+    $('#clock').removeClass();
+    $('#clock').addClass('black');
+  });
+
+  $('#yellow').click( function() {
+    $('#clock').removeClass();
+    $('#clock').addClass('yellow');
+  });
+
+  $('#default').click( function() {
+    $('#clock').removeClass();
+    $('#clock').addClass('default');
+  });
+});
