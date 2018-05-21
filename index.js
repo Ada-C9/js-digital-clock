a5c11c2937a5ac6d74443be37a7779df5f160e68
Mary Lamkin <mmlamkin@gmail.com>
2018-05-21 08:37:40 -0700

    fixes undefined for minutes and seconds

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+
+const DigitalClock = function DigitalClock () {
+  const start = new Date();
+
+  let hours = start.getHours();
+  let minutes = start.getMinutes();
+  let seconds = start.getSeconds();
+
+  const addZeros = function addZeros(num) {
+    if (num < 10) {
+      num = `0${num}`;
+    }
+    return num
+  }
+
+  minutes = addZeros(minutes)
+  seconds = addZeros(seconds)
+
+  const target = document.getElementById('clock')
+
+  target.innerHTML = `${hours}:${minutes}:${seconds}`;
+}
+
+setInterval(DigitalClock, 1000);
