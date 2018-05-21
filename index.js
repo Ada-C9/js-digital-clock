d4c74d4fb283cec484db4ec3b93bbb734a124841
Tor Shimizu <torshimizu@gmail.com>
2018-05-21 16:43:22 -0700

    refactored and added multiple city clocks

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 const getTime = () => {
   let newTime = new Date();
+  $('#seattle-clock > .city-clock').html(newTime);
+};
 
-  $('.city-clock').html(newTime.toString());
+const timeBuilder = () => {
+  let newTime = new Date();
+  let utc = newTime.getTime()// + (newTime.getTimezoneOffset() * 60000);
+  return utc
 }
 
+const guamTime = () => {
+  let utc = timeBuilder()
+  let calculatedTime = new Date(utc + (3600000 * 17))
+
+  $('#guam-clock > .city-clock').html(calculatedTime);
+};
+
+const hawaiiTime = () => {
+  let utc = timeBuilder()
+  let calculatedTime = new Date(utc + (3600000 * -3))
+
+  $('#hawaii-clock > .city-clock').html(calculatedTime);
+};
+
+const londonTime = () => {
+  let utc = timeBuilder()
+  let calculatedTime = new Date(utc + (3600000 * 8))
+
+  $('#london-clock > .city-clock').html(calculatedTime);
+};
 
 
 
 
 $(document).ready(() => {
-  setInterval(getTime(), 1000);
+  setInterval(getTime, 1000);
+  setInterval(guamTime, 1000);
+  setInterval(hawaiiTime, 1000);
+  setInterval(londonTime, 1000);
 });
