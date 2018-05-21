bdc44aeee08171e010c2bb53b027a1eac9ff1e41
Victoria Garcia <v.elisabeth.garcia@gmail.com>
2018-05-21 06:38:26 -0700

    very basic clock is appearing in the browser, but it is not refreshing yet.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+
+
+console.log('This is a test');
+
+let target = document.getElementById('clock');  // Find the HTML element where the ID is js-lecture-target
+
+let now = Date.now();
+let nowDate = new Date(now);
+let nowHour = nowDate.getHours();
+let nowMinute = nowDate.getMinutes();
+let nowSecond = nowDate.getSeconds();
+
+let timeReport = `${nowHour}:${nowMinute}:${nowSecond}`
+
+target.innerHTML = timeReport
