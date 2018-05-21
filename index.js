a38928861d5a8ee0331b34de1f5f5c6825a22854
Kate Pond <kate@thepondsedge.com>
2018-05-21 15:13:39 -0700

    makes color buttons so that you can color the clock the way you want to

--- index.js
+++ index.js
@@ -12,14 +12,20 @@ $( document ).ready(() => {
 
   $("#blue-button").click(function() {
     $('#clock').toggleClass('blue');
+    $('#clock').removeClass('purple');
+    $('#clock').removeClass('gold');
   });
 
   $("#purple-button").click(function() {
     $('#clock').toggleClass('purple');
+    $('#clock').removeClass('blue');
+    $('#clock').removeClass('gold');
   });
 
   $("#gold-button").click(function() {
     $('#clock').toggleClass('gold');
+    $('#clock').removeClass('purple');
+    $('#clock').removeClass('blue');
   });
 
   window.setInterval(clock, 1000);
