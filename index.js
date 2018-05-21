e9de6476f5f3fdbeb36744da6d6e934596fff275
Jamila <J.cornick21@gmail.com>
2018-05-21 08:31:10 -0700

    hw submission

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 // Your code here
 let time = new Date();
+// let hourdiff = 3
+// let timediff = hourdiff * 60 + time.getTimezoneOffset();
 let intervalID = setInterval(() => {
   $('#clock').html(new Date())
 }, 1000);
 
 
 // function updateTime() {
-// $('#clock').append(time)
+// $('#timeZone1').html(time)
 // }
+
+
 
 console.log('YougotTime');
 $(document).ready(() => {
