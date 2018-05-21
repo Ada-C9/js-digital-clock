0113a2a78676a13805a31fed6c0ffc79f1833a07
Samantha Berk <sberk10@gmail.com>
2018-05-21 16:39:10 -0700

    change times to 24 hrs

--- index.js
+++ index.js
@@ -21,51 +21,106 @@ ShanghaiTime.setMinutes(ShanghaiTime.getMinutes() + ShanghaiOffset);
 // set the interval to increment by 1 second
 const interval = 1000;
 
-function updateTime() {
+function updateTime24() {
   SeattleTime = new Date(SeattleTime.getTime() + interval);
 
   // slice the GMT-0700 off the date object
   $('#seattle').html(`<strong>Seattle, WA: </strong><br>${SeattleTime.toString().split(' ').slice(0, 5).join(' ')}`);
 }
 
-function updateDCTime() {
+function updateDCTime24() {
   DCTime = new Date(DCTime.getTime() + interval);
   $('#dc').html(`<strong>Washington, DC: </strong><br>${DCTime.toString().split(' ').slice(0, 5).join(' ')}`);
 }
 
-function updateCairoTime() {
+function updateCairoTime24() {
   CairoTime = new Date(CairoTime.getTime() + interval);
   $('#cairo').html(`<strong>Cairo, Egypt: </strong><br>${CairoTime.toString().split(' ').slice(0, 5).join(' ')}`);
 }
 
-function updateShanghaiTime() {
+function updateShanghaiTime24() {
   ShanghaiTime = new Date(ShanghaiTime.getTime() + interval);
-  $('#shanghai').html(`<strong>Shanghai, China: </strong><br>
+  $('#shanghai').html(`<strong>Shanghai, China: </strong><br>\n
     ${ShanghaiTime.toString().split(' ').slice(0, 5).join(' ')}`);
   }
 
-  function format12HourTime(time) {
-    let hours = time.getHours();
-    let minutes = time.getMinutes();
-    let ampm = hours >= 12 ? 'pm' : 'am';
-    hours = hours % 12;
-    hours = hours ? hours : 12;
-    minutes = minutes < 10 ? '0' + minutes : minutes;
-    let strTime = time.toString().split(' ').slice(1, 4).join(' ');
-    strTime = strTime + hours + ':' + minutes + ' ' + ampm;
-    return strTime;
-  }
+// update times for 12 hour format
+function updateTime12() {
+  SeattleTime = new Date(SeattleTime.getTime() + interval);
+  let hours = SeattleTime.getHours();
+  let minutes = SeattleTime.getMinutes();
+  let ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  let strTime = location.toString().split(' ').slice(1, 4).join(' ');
+  strTime = strTime + ' ' + hours + ':' + minutes + ' ' + ampm;
+  $('#seattle').html(`<strong>Seattle, WA: </strong>${strTime}<br>`);
+}
+
+function updateDCTime12() {
+  DCTime = new Date(DCTime.getTime() + interval);
+  let hours = DCTime.getHours();
+  let minutes = DCTime.getMinutes();
+  let ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  let strTime = location.toString().split(' ').slice(1, 4).join(' ');
+  strTime = strTime + ' ' + hours + ':' + minutes + ' ' + ampm;
+  $('#dc').html(`<strong>Washington, DC: </strong>${strTime}<br>`);
+}
+
+function updateCairoTime12() {
+  CairoTime = new Date(CairoTime.getTime() + interval);
+  let hours = CairoTime.getHours();
+  let minutes = CairoTime.getMinutes();
+  let ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  let strTime = location.toString().split(' ').slice(1, 4).join(' ');
+  strTime = strTime + ' ' + hours + ':' + minutes + ' ' + ampm;
+  $('#cairo').html(`<strong>Cairo, Egypt: </strong>${strTime}<br>`);
+}
+
+function updateShanghaiTime12() {
+  ShanghaiTime = new Date(ShanghaiTime.getTime() + interval);
+  let hours = ShanghaiTime.getHours();
+  let minutes = ShanghaiTime.getMinutes();
+  let ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  let strTime = location.toString().split(' ').slice(1, 4).join(' ');
+  strTime = strTime + ' ' + hours + ':' + minutes + ' ' + ampm;
+  $('#shanghai').html(`<strong>Shanghai, China: </strong>${strTime}<br>`);
+}
 
   $(document).ready(() => {
-    updateTime();
-    setInterval(updateTime, interval);
-    updateDCTime();
-    setInterval(updateDCTime, interval);
-    updateCairoTime();
-    setInterval(updateCairoTime, interval);
-    updateShanghaiTime();
-    setInterval(updateShanghaiTime, interval);
+    if ($('body').hasClass('24-hours')) {
+      updateTime24();
+      setInterval(updateTime24, interval);
+      updateDCTime24();
+      setInterval(updateDCTime24, interval);
+      updateCairoTime24();
+      setInterval(updateCairoTime24, interval);
+      updateShanghaiTime24();
+      setInterval(updateShanghaiTime24, interval);
+    }
+
+    if ($('body').hasClass('12-hours')) {
+      updateTime12();
+      setInterval(updateTime12, interval);
+      updateDCTime12();
+      setInterval(updateDCTime12, interval);
+      updateShanghaiTime12();
+      setInterval(updateShanghaiTime12, interval);
+      updateCairoTime12();
+      setInterval(updateCairoTime12, interval);
+    }
   });
+
 
   $(document).ready(() => {
     $('button').click ( function() {
@@ -83,8 +138,10 @@ function updateShanghaiTime() {
     $('.time-btn').click ( function() {
       if($(this).html() === '12 hours') {
         $(this).html('24 hours');
+        $('body').addClass('24-hours');
       } else {
         $(this).html('12 hours')
+        $('body').addClass('12-hours');
       }
     });
   });
