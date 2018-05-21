a8bac81fb9b63072c17bafb4a9ca135471eb7c48
Jackie <jonofre16@hotmail.com>
2018-05-21 10:57:12 -0700

    Updated d variable to getDate vs getDay

--- index.js
+++ index.js
@@ -1,34 +1,49 @@
+const COLORS = ['black', 'pink', 'green', 'indigo'];
+let colorIndex = 0;
+
+const nextColor = function nextColor() {
+  let color = COLORS[colorIndex];
+  colorIndex += 1;
+  colorIndex %= COLORS.length;
+  return color;
+};
+
 $(document).ready(function() {
+  $('body').keydown(function(event){
+    if (event.which == 13 || event.which == 32) {
+      $('#body').toggleClass(nextColor());
+    }
+  });
 
-function readTime() {
-let dt = new Date();
+  function readTime() {
+    let dt = new Date();
 
-let month = new Array();
-month[0] = "January";
-month[1] = "February";
-month[2] = "March";
-month[3] = "April";
-month[4] = "May";
-month[5] = "June";
-month[6] = "July";
-month[7] = "August";
-month[8] = "September";
-month[9] = "October";
-month[10] = "November";
-month[11] = "December";
+    let month = new Array();
+    month[0] = "January";
+    month[1] = "February";
+    month[2] = "March";
+    month[3] = "April";
+    month[4] = "May";
+    month[5] = "June";
+    month[6] = "July";
+    month[7] = "August";
+    month[8] = "September";
+    month[9] = "October";
+    month[10] = "November";
+    month[11] = "December";
 
-let m = month[dt.getMonth()];
-let d = dt.getDay();
-let y = dt.getFullYear();
-let hr = dt.getHours();
-let min = dt.getMinutes();
-let sec = dt.getSeconds();
-
-
-let display = m + ' ' + d + ',' + ' ' + y + '<br>' + hr + ":" + min + ":" + sec;
-$('#clock').html(`${display}`);
-}
+    let m = month[dt.getMonth()];
+    let d = dt.getDate();
+    let y = dt.getFullYear();
+    let hr = dt.getHours();
+    let min = dt.getMinutes();
+    let sec = dt.getSeconds();
 
 
-setInterval(readTime, 1000);
+    let display = m + ' ' + d + ',' + ' ' + y + '<br>' + hr + ":" + min + ":" + sec;
+    $('#clock').html(`${display}`);
+  }
+
+
+  setInterval(readTime, 1000);
 });
