20b4cc870c634d4dc1211bd75af4bc7745fd1016
Karinna Iniguez <karinnainiguez@gmail.com>
2018-05-21 08:51:05 -0700

    refactor

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
-// Your code here
-console.log('this is loading');
+
+const new_date = () => new Date();
+
+const parseDate = (dateObj) => {
+
+  const localDate = dateObj.toLocaleString(
+    'en-US',{
+      weekday: 'long',
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric'
+    }
+  );
+
+  const localTime = dateObj.toLocaleString(
+    'en-US',{
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+      timeZoneName: 'short'
+    }
+  );
+
+  $('#clock').html(`<div>${localDate}</div>`)
+  $('#clock').append(`<div>${localTime}</div>`)
+};
+
+
+
+$(document).ready(() => {
+  setInterval(function() { parseDate(new_date())}, 1000);
+})
