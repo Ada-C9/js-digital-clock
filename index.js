0a82784e3bf03c23a638e1c8143e001b9ef01476
Karinna Iniguez <karinnainiguez@gmail.com>
2018-05-21 13:36:41 -0700

    Adding color and hour buttons

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 
 const new_date = () => new Date();
 
-const parseDate = (dateObj) => {
+let hoursVar = true;
+
+const parseDate = (dateObj, hours=hoursVar) => {
 
   const localDate = dateObj.toLocaleString(
     'en-US',{
@@ -14,6 +16,7 @@ const parseDate = (dateObj) => {
 
   const localTime = dateObj.toLocaleString(
     'en-US',{
+      hour12: hours,
       hour: 'numeric',
       minute: 'numeric',
       second: 'numeric',
@@ -21,12 +24,26 @@ const parseDate = (dateObj) => {
     }
   );
 
-  $('#clock').html(`<div>${localDate}</div>`)
-  $('#clock').append(`<div>${localTime}</div>`)
+  $('#clock').html(`<div>${localDate}</div>`);
+  $('#clock').append(`<div>${localTime}</div>`);
 };
 
 
 
 $(document).ready(() => {
   setInterval(function() { parseDate(new_date())}, 1000);
+
+  $('.color').click(function(){
+    $('body').removeClass();
+    $('body').addClass(this.outerText);
+  });
+
+  $('.hours').click(function(){
+    if (hoursVar) {
+      hoursVar = false;
+    } else {
+      hoursVar = true;
+    }
+  });
+
 })
