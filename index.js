e8b8e1cedc22b0c7c62134735f3d7e8222eda617
Winifred Irarrazaval <winirarrazaval@gmail.com>
2018-05-21 12:55:40 -0700

    Still working on the am pm, does change it to PMAM but can not reverse

--- index.js
+++ index.js
@@ -29,26 +29,50 @@ const LOCATIONS = [
 ];
 
 
-  const updateChile = () => {
-    LOCATIONS.forEach((location) => {
-      let chileTime = new Date();
-      chileTime.setHours(chileTime.getHours() + location.hours);
+
+const updateChile = () => {
+  LOCATIONS.forEach((location) => {
+    let currenttime = new Date();
+    currenttime.setHours(currenttime.getHours() + location.hours);
+    if ($(`.timeshow`).hasClass(12)) {
       $(`#`+location.place.toLowerCase()+`clock`).html(
-        chileTime.getHours() + ":" + chileTime.getMinutes() + ":" + chileTime.getSeconds()
-      );
-      $(`#`+location.place.toLowerCase()+`clock`).append(
-        "<br>" + location.place + "</br>"
+        currenttime.toLocaleString([], { hour12: true}))
+      }
+      else {
+        $(`#`+location.place.toLowerCase()+`clock`).html(
+          currenttime.getHours() + ":" + currenttime.getMinutes() + ":" + currenttime.getSeconds()
+        );}
+
+        $(`#`+location.place.toLowerCase()+`clock`).append(
+          "<br>" + location.place + "</br>"
+        );}
       );}
-    );}
+      // toLocaleString([], { hour12: true});
+
+      setInterval(updateClock, 1000);
+      setInterval(updateChile, 1000);
+
+      $(`.coloring`).on(`click`, `button`,function(event) {
+        console.log(event.target.innerHTML.toLowerCase());
+        $(`body`).removeClass();
+        $(`body`).addClass(event.target.innerHTML.toLowerCase());
+
+      })
+
+      $(`.time`).on(`click`,`button`, function(event){
+        if (event.target.innerHTML == "AM/PM"){
+      
+          $(`.timeshow`).addClass(`12`)
+        }
+        else {
+
+          $(`.timeshow`).addClass(`24`)
+        }
+      })
 
 
-  setInterval(updateClock, 1000);
-  setInterval(updateChile, 1000);
 
-
-
-
-  $(document).ready(()=> {
-    updateClock();
-    updateChile();
-  });
+      $(document).ready(()=> {
+        updateClock();
+        updateChile();
+      });
