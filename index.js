40a7475adc1f2ec087e8016a36856479c7d748b3
Aruna <aruna.thungathurthi@gmail.com>
2018-05-20 22:17:16 -0700

    added a method update clocks

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+
+      const clocks = document.getElementsByClassName("clock");
+
+      function updateClocks() {
+        for (let clock of clocks) {
+          let timezone = clock.dataset.timezone;
+          let time = new Date().toLocaleTimeString("en-US", {
+            hour: '2-digit',
+            minute:'2-digit',
+            second:'2-digit',
+            timeZone: timezone
+          });
+          clock.textContent = time;
+        }
+      }
+
+
+  setInterval(updateClocks, 1000);
+  
+  updateClocks();
