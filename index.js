737119f7d4769c5cd0df7962067e2d8d487d0700
Madaleine Shields <maddiev.shields@gmail.com>
2018-05-22 12:32:13 -0700

    finish clock

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-// Your code here
+
+
+$(document).ready(function (){ setInterval(function(){ myTimer() }, 1000);
+
+
+function myTimer() {
+    let d = new Date();
+    let t = d.toLocaleTimeString();
+    $('#clock').text(t);
+    // $('#clock').append(-d.getTimezoneOffset() / 60);
+
+    // $('#clock').text(t);
+}
+
+// $("#blue-button").click(function() {
+//   $('#clock').toggleClass('blue');
+// });
+//
+// $("#purple-button").click(function() {
+//   $('#clock').toggleClass('purple');
+// });
+//
+// $("#gold-button").click(function() {
+//   $('#clock').toggleClass('gold');
+// });
+
+
+
+
+
+$("#blue-button").click(function() {
+    $('#clock').toggleClass('blue');
+    $('#clock').removeClass('purple');
+    $('#clock').removeClass('gold');
+  });
+
+  $("#purple-button").click(function() {
+    $('#clock').toggleClass('purple');
+    $('#clock').removeClass('blue');
+    $('#clock').removeClass('gold');
+  });
+
+  $("#gold-button").click(function() {
+    $('#clock').toggleClass('gold');
+    $('#clock').removeClass('purple');
+    $('#clock').removeClass('blue');
+  });
+
+
+
+
+});
