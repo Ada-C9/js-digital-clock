0e38b758ad29c180fb7c2bd78445250917ddf6ce
Phoebe <phoebe.manning8@gmail.com>
2018-05-22 12:27:22 -0700

    digital clock

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Your code here
+$(document).ready( function (){
+
+  let militaryTime = true;
+
+  $('#time-button').on('click', function(){
+    militaryTime = !militaryTime;
+  });
+
+  $('#color-button').on('click', function(){
+    $('div').toggleClass('new-color');
+  });
+
+  setInterval( function(){ timeFn() }, 1000 );
+
+  function timeFn() {
+    let date = new Date();
+    let time = date.toLocaleTimeString('en-US', { hour12: militaryTime });
+    $('.clock').text(time);
+  }
+
+});
