7a3f159e5093f4730d859b3edd3de1bdab12247f
Brenda Rios <riosbrenda@gmail.com>
2018-05-22 12:18:43 -0700

    Added colors functionality.

--- index.js
+++ index.js
@@ -5,8 +5,30 @@ function getTime() {
 $('#clock').html(localTime);
 }
 
+
 $(document).ready(() => {
   setInterval(getTime, 1000);
 
+  $('#purple').on('click', function() {
+      let target = $("#clock");
+      target.removeClass();
+      target.addClass('purple');
+  });
+
+  $('#red').on('click', function() {
+      let target = $("#clock");
+      target.removeClass();
+      target.addClass('red');
+  });
+
+  $('#green').on('click', function() {
+      let target = $("#clock");
+      target.removeClass();
+      target.addClass('green');
+  });
+
+  // $('#display').on('click', function() {
+  //     document.write(new Date().toString("hh:mm tt"));
+  // });
 
 });
